e173ccb0a9135b4d0346b1afd70bf5ef3ddf88e7
agate-pris <agate-pris@users.noreply.github.com>
2024-03-05 22:48:16 +0900

    MathTests.TestRepeat を追加

--- Tests/MathTests.cs
+++ Tests/MathTests.cs
@@ -1,4 +1,16 @@
+using NUnit.Framework;
+
 namespace AgatePris.UnityUtility {
     public static class MathTests {
+        [TestCase(9, -11, 10)]
+        [TestCase(0, -10, 10)]
+        [TestCase(1, -9, 10)]
+        [TestCase(9, -1, 10)]
+        [TestCase(0, 0, 10)]
+        [TestCase(1, 1, 10)]
+        [TestCase(9, 9, 10)]
+        [TestCase(0, 10, 10)]
+        [TestCase(1, 11, 10)]
+        public static void TestRepeat(int expected, int value, int length) => Assert.AreEqual(expected, Math.Repeat(value, length));
     }
 }
