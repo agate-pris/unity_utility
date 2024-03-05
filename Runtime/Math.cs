9a9e471160ce99980da5ed5f2f9f04c306a9f7cb
agate-pris <agate-pris@users.noreply.github.com>
2024-03-05 22:49:00 +0900

    Math.Repeat を追加

--- Runtime/Math.cs
+++ Runtime/Math.cs
@@ -1,4 +1,14 @@
+using System.Runtime.CompilerServices;
+
 namespace AgatePris.UnityUtility {
     public static partial class Math {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Repeat(int value, int length) {
+            var rem = value % length;
+            if (rem < 0) {
+                return rem + length;
+            }
+            return rem;
+        }
     }
 }
