d7136153f338782f173d40a1350c0915832a6213
agate-pris <agate-pris@users.noreply.github.com>
2024-03-10 01:16:36 +0900

    EnumerableTests.TestOnce を追加

--- Tests/EnumerableTests.cs
+++ Tests/EnumerableTests.cs
@@ -1,4 +1,14 @@
+using NUnit.Framework;
+using System.Linq;
+using static AgatePris.UnityUtility.Enumerable;
+
 namespace AgatePris.UnityUtility {
     public static class EnumerableTests {
+        [Test]
+        public static void TestOnce() {
+            var l = Once(1).ToList();
+            Assert.AreEqual(1, l.Count);
+            Assert.AreEqual(1, l[0]);
+        }
     }
 }
