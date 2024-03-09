5c713327f4a7b71d33265a3991fbbe0c4d88d3e1
agate-pris <agate-pris@users.noreply.github.com>
2024-03-10 01:22:07 +0900

    EnumerableTests.TestCycle を追加

--- Tests/EnumerableTests.cs
+++ Tests/EnumerableTests.cs
@@ -10,5 +10,16 @@ namespace AgatePris.UnityUtility {
             Assert.AreEqual(1, l.Count);
             Assert.AreEqual(1, l[0]);
         }
+
+        [Test]
+        public static void TestCycle() {
+            var l = new[] { 1, 2, 3 }.Cycle().Take(5).ToList();
+            Assert.AreEqual(5, l.Count);
+            Assert.AreEqual(1, l[0]);
+            Assert.AreEqual(2, l[1]);
+            Assert.AreEqual(3, l[2]);
+            Assert.AreEqual(1, l[3]);
+            Assert.AreEqual(2, l[4]);
+        }
     }
 }
