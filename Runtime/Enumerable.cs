c873ada694ebcf433a1f9b33389f04c368af8d05
agate-pris <agate-pris@users.noreply.github.com>
2024-03-10 01:25:50 +0900

    Enumerable.Cycle を追加

--- Runtime/Enumerable.cs
+++ Runtime/Enumerable.cs
@@ -1,9 +1,21 @@
+using System;
 using System.Collections.Generic;
 
 namespace AgatePris.UnityUtility {
     public static class Enumerable {
         public static IEnumerable<T> Once<T>(T value) {
             yield return value;
+        }
+        public static IEnumerable<T> Cycle<T>(this IEnumerable<T> source) {
+            if (source == null) {
+                throw new ArgumentNullException("source");
+            }
+            while (true) {
+                using var e = source.GetEnumerator();
+                while (e.MoveNext()) {
+                    yield return e.Current;
+                }
+            }
         }
     }
 }
