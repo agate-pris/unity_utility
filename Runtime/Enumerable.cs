e33e2d0eeaba12d3f812429a2fe576b597cc0075
agate-pris <agate-pris@users.noreply.github.com>
2024-03-10 01:14:26 +0900

    Enumerable.Once を追加

--- Runtime/Enumerable.cs
+++ Runtime/Enumerable.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+
 namespace AgatePris.UnityUtility {
     public static class Enumerable {
+        public static IEnumerable<T> Once<T>(T value) {
+            yield return value;
+        }
     }
 }
